ca6e5c14d2159889aaab06a38d4fa2aa94ce0274
samitkumarpatel <samitkumarpatel@gmail.com>
2025-01-18 22:57:53 +0100

    more details added

---
+++ order-api/src/test/java/net/samitkumar/order_api/OrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.samitkumar.order_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
